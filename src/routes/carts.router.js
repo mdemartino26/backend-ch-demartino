fd6aff03f4245beec8b705895015b89d8b411fc4
mdemartino26 <m.demartino99@gmail.com>
2024-01-30 23:45:46 -0300

    post al carito con error

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -38,6 +38,20 @@ router.get("/:cid", async (req, res) => {
   }
 });
 
+router.post("/:cid/product/:pid", async (req, res) => {
+  const cartId = parseInt(req.params.cid);
+  const productId =req.params.pid;
+  const quantity = req.body.quantity || 1;
+
+  try{
+    const actualizarCarrito = await cartManager.addProductToCart(cartId, productId, quantity = 1);
+    res.json(actualizarCarrito.products);
+  }catch(error){
+    console.error("Error al agregar producto al carrito ", error);
+    res.status(500).json({error: "Error interno del servidor"});
+  }
+});
+
 
 module.exports = router;
 
