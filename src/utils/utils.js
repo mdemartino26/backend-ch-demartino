bf6627f0b15ec813d1c2602ec0bf8a599fdbee07
mdemartino26 <m.demartino99@gmail.com>
2024-05-03 00:37:15 -0300

    saque dos lienas vacias

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,6 +5,4 @@ const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 
 const isValidPassword = (password, hashedPassword) => bcrypt.compareSync(password, hashedPassword);
 
-
-
 module.exports = {createHash, isValidPassword};
